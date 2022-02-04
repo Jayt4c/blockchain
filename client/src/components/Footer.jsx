981ba11925bafd8a47db37ccc3f1b16e006575b7
BossTI <igejuwon53@gmail.com>
2022-02-03 16:39:34 -0800

    new

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import logo from "../../images/jaywebdev.jpg";
+import logo from "../../images/jaywebdev.jpg"; 
 
 const Footer = () => (
   <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
